acaac78cb897ce9ff134c0e49c7e431e52c44aa4
KostaLekos <github.shiny670@passmail.com>
2026-02-20 14:23:25 -0800

    First commit

---
+++ CommonDenominator.java
@@ -0,0 +1,19 @@
+public class CommonDenominator {
+	import java.util.Scanner;
+
+	public int findCommonDenominator(int den1, int den2) {
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in());
+
+		System.out.println("Please enter a fraction ('x/y' format):\n");
+		String frac1 = input.nextLine();
+
+		System.out.println("Now enter another fraction to add to the first:\n");
+		String frac2 = input.nextLine();
+
+
+	}
+}
