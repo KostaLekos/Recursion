01b03760bf869a59b4151398f203f2c9752c9f84
KostaLekos <github.shiny670@passmail.com>
2026-02-24 17:49:53 -0800

    Made recursion more preformant

--- Pascal.java
+++ Pascal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Pascal {
-    public static int[] genPascal(int line) {
+    public static int[] printPascal(int line) {
         int[] toReturn = new int[line];
         int[] prevLine;
 
@@ -10,7 +10,7 @@ public class Pascal {
         } else if (line == 1) {
             toReturn[0] = 1;
         } else {
-            prevLine = genPascal(line - 1);
+            prevLine = printPascal(line - 1);
 
             for (int i = 1; i < line - 1; i++) {
                 toReturn[i] = prevLine[i - 1] + prevLine[i];
@@ -19,6 +19,15 @@ public class Pascal {
             toReturn[0] = 1;
             toReturn[line - 1] = 1;
         }
+
+        for (int j = 0; j < line; j++) {
+            System.out.print(toReturn[j]);
+            if (j != line - 1) {
+                System.out.print(" "); // space between numbers
+            }
+        }
+        System.out.println();
+
         return toReturn;
     }
 
@@ -33,19 +42,6 @@ public class Pascal {
         int lineNum = Integer.valueOf(input.nextLine());
         System.out.println();
 
-        int[] line;
-        for (int i = 1; i <= lineNum; i++) {
-            System.out.print(" ");
-            line = genPascal(i);
-
-            for (int j = 0; j < line.length; j++) {
-                System.out.print(line[j]);
-                if (j != line.length - 1) {
-                    System.out.print(" "); // space between numbers
-                }
-            }
-            System.out.println();
-        }
-        input.close();
+        printPascal(lineNum);
     }
 }
